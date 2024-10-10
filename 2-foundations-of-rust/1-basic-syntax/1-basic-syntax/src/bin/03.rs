88b2c5c61991bb096a8c931e62cbe4c3c0e59cce
Otilia <otilia.rudnic@stud.acs.upb.ro>
2024-10-10 19:28:57 +0300

    Laborator 1

--- 2-foundations-of-rust/1-basic-syntax/1-basic-syntax/src/bin/03.rs
+++ 2-foundations-of-rust/1-basic-syntax/1-basic-syntax/src/bin/03.rs
@@ -2,6 +2,16 @@ fn main() {
     let input = [23, 82, 16, 45, 21, 94, 12, 34];
 
     // TODO
-
-    println!("{} is largest and {} is smallest");
+    let n = input.len() - 1;
+    let mut smallest = input[0];
+    let mut largest = input[n];
+    for i in 0..n {
+        if input[i] < smallest {
+            smallest = input[i];
+        }
+        if input[i] > largest {
+            largest = input [i];
+        }
+    }
+    println!("{} is largest and {} is smallest", largest, smallest);
 }
